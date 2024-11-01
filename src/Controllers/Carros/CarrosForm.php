0357f46aeef47a23e75a0b15a108a10ddeb78a7c
Rivas117 <167035998+Rivas117@users.noreply.github.com>
2024-10-31 18:24:01 -0600

    Cambio 5
    Mejora de Interfaz y mejoras en el View y Eliminar

--- src/Controllers/Carros/CarrosForm.php
+++ src/Controllers/Carros/CarrosForm.php
@@ -92,8 +92,16 @@ class CarrosForm extends PublicController
                 }
                 break;
             case "UPD":
+                $result = Carros::actualizarCarro($this->carro);
+                if ($result) {
+                    Site::redirectToWithMsg("index.php?page=Carros-CarrosList", "Carro Actualizado Satisfactoriamnete");
+                }
                 break;
             case "DEL":
+                $result = Carros::eliminarCarro($this->carro["codigo"]);
+                if ($result) {
+                    Site::redirectToWithMsg("index.php?page=Carros-CarrosList", "Carro Eliminado Satisfactoriamnete");
+                }
                 break;
         }
     }
@@ -106,5 +114,11 @@ class CarrosForm extends PublicController
             $this->carro["codigo"]
         );
         $this->viewData["carro"] = $this->carro;
+        $this->viewData["readonly"] =
+            ($this->viewData["mode"] === "DEL"
+                || $this->viewData["mode"] === "DSP")
+            ? "readonly" : "";
+
+        $this->viewData["showConfirm"] = ($this->viewData["mode"] !== "DSP");
     }
 }
